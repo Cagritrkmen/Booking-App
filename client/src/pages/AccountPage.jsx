e703950e5bb0af74a9bb60f721399025a9bb7e5b
Cagritrkmen <cagritrkmn33@gmail.com>
2024-07-09 12:03:42 +0300

    Complete Login and Logout

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,24 +1,52 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { UserContext } from '../UserContext'
-import { Link, Navigate } from 'react-router-dom'
+import { Link, Navigate, useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const AccountPage = () => {
-  const {user,ready} = useContext(UserContext)
+  const { user, ready , setUser} = useContext(UserContext)
+  const [redirectToHome, setRedirectToHome] = useState(false);
 
-  if(!ready){
+  let { subpage } = useParams();
+  if (subpage == undefined) {
+    subpage = "profile"
+  }
+  async function logout() {
+    await axios.post("/logout")
+    setRedirectToHome(true);
+    setUser(null)
+  
+  }
+  if (redirectToHome) {
+    return <Navigate to="/" />;
+  }
+  if (!ready) {
     return "Loading..."
   }
 
-  if(ready&&!user){
-    return <Navigate  to={"/login"}></Navigate>
+  if (ready && !user) {
+    return <Navigate to={"/login"}></Navigate>
+  }
+  function linkClasses(type = null) {
+    let classes = "py-2 px-6";
+    if (type == subpage) {
+      classes += " bg-primary text-white rounded-full";
+    }
+    return classes
   }
   return (
     <div>
-      <nav className='w-full flex  justify-center mt-8 gap-4'>
-        <Link className='py-2 px-6 bg-primary text-white rounded-full' to={"/account"}>My Profile</Link>
-        <Link className='py-2 px-6' to={"/account/bookings"}>My Bookings</Link>
-        <Link className='py-2 px-6' to={"/account/places"}>My Accommodations</Link>
+      <nav className='w-full flex  justify-center mt-8 gap-4 mb-10'>
+        <Link className={linkClasses("profile")} to={"/account"}>My Profile</Link>
+        <Link className={linkClasses("bookings")} to={"/account/bookings"}>My Bookings</Link>
+        <Link className={linkClasses("places")} to={"/account/places"}>My Accommodations</Link>
       </nav>
+      {subpage === "profile" && (
+        <div className="text-center max-w-lg mx-auto">
+          Logged in as {user.name} ({user.email}) <br></br>
+          <button onClick={logout} className='primary max-w-xs mt-2'>Logout</button>
+        </div>
+      )}
     </div>
   )
 }
